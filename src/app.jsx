0d2835dd5948615fc1eb0b81c3a1a38d6451d9b9
Lee-SeongHyun316 <mn0316@naver.com>
2021-09-07 14:13:00 +0900

    [ADD] state move : habits -> app

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,49 @@
 import './app.css';
 import Habits from "./components/habits";
-function App() {
-    return <Habits/> ;
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    state = {
+        habits: [
+            {id: 1, name: 'Reading', count: 0},
+            {id: 2, name: 'Pilates', count: 0},
+            {id: 3, name: 'Web Development', count: 0},
+        ],
+    };
+
+    handleIncrement = habit => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({habits});
+    };
+
+    handleDecrement = habit => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({habits});
+    };
+
+    handleDelete = habit => {
+        const habits = this.state.habits.filter(item => item.id !== habit.id);
+        this.setState({habits});
+    };
+
+    render() {
+        return (
+            <Habits
+                habits={this.state.habits}
+                onIncrement={this.handleIncrement}
+                onDecrement={this.handleDecrement}
+                onDelete = {this.handleDelete}
+            >
+            </Habits>
+        );
+    }
 }
+
+
 export default App;
